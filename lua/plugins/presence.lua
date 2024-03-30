9d5f35f0189970a54fdded457607ec40566b6d93
Jhoan Manuel Buitrago Chavez <jhoanuitrago@gmail.com>
2024-03-30 14:57:58 -0500

    Change presence.nvim to a more updated fork

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -1,9 +1,14 @@
 return {
-  "andweeb/presence.nvim",
-  config = function()
-    require("presence").setup({
-      auto_update = true,
-      main_image = "neovim",
-    })
-  end
+  -- Use a fork of the official plugin since its more updated
+  "jiriks74/presence.nvim",
+  event = "UIEnter",
+  opts = function(_, opts)
+    opts.neovim_image_text = "Configuring neovim is such a pain"
+  end,
+  --config = function()
+  --  require("presence").setup({
+  --    auto_update = true,
+  --    main_image = "neovim",
+  --  })
+  --end
 }
