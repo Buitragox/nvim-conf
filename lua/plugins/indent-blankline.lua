5a7c6b72fdb4903e1f2e5f39dc0b1c56aa689238
Jhoan Manuel Buitrago Chavez <jhoanuitrago@gmail.com>
2024-04-02 10:36:44 -0500

    Add plugins for comments and indent guides

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,7 @@
+return {
+  -- https://github.com/lukas-reineke/indent-blankline.nvim
+  -- Adds indentation guides to neovim
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {},
+}
