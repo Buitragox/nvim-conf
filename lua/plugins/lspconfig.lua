afe63c527ff18764837d68bc0db529fc4f78047b
Jhoan Manuel Buitrago Chavez <jhoanuitrago@gmail.com>
2024-04-01 22:44:57 -0500

    Add a bit of documentation

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,6 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
+        -- Add here lsps that should be installed
         ensure_installed = { "lua_ls", "ruff_lsp", "solargraph" },
       })
     end,
@@ -17,6 +18,7 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
+      -- Setup lsps
       lspconfig.lua_ls.setup({})
       lspconfig.ruff_lsp.setup({})
       lspconfig.solargraph.setup({})
