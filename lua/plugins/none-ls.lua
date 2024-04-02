8b5c699ae7e9e9aa8a1ac227da3be17aa4021f87
Jhoan Manuel Buitrago Chavez <jhoanuitrago@gmail.com>
2024-04-01 22:49:56 -0500

    Add format on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,7 +8,24 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.rubocop,
         null_ls.builtins.diagnostics.rubocop,
-      }
+      },
+
+      -- This enables formatting on save.
+      -- https://github.com/nvimtools/none-ls.nvim/wiki/Formatting-on-save
+      -- You can disable this by changing 'enabled' to false
+      on_attach = function(client, buffer_n)
+        local enabled = true
+        if client.supports_method("textDocument/formatting") and enabled then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            desc = "Format on save",
+            group = vim.api.nvim_create_augroup("format-on-save", { clear = true }),
+            buffer = buffer_n,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
     })
-  end
+  end,
 }
