7d0c6c90ff40146589d07a26694c66b060e45ed6
Jhoan Manuel Buitrago Chavez <jhoanuitrago@gmail.com>
2024-05-16 09:08:02 -0500

    refactor: change the lspconfig and add TODO

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -19,13 +19,69 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
+      -- TODO: Make cursor hover show definition on floating box
+
       -- Setup lsps
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
-      -- lspconfig.ruff_lsp.setup({ capabilities = capabilities })
-      lspconfig.solargraph.setup({ capabilities = capabilities })
-      lspconfig.pyright.setup({ capabilities = capabilities })
+      -- config from https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        on_init = function(client)
+          local path = client.workspace_folders[1].name
+          if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+            return
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                -- Depending on the usage, you might want to add additional paths here.
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              },
+            },
+          })
+        end,
+        settings = {
+          Lua = {},
+        },
+      })
+
+      lspconfig.ruff_lsp.setup({})
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        settings = {
+          pyright = {
+            -- Using Ruff's import organizer
+            disableOrganizeImports = true,
+          },
+          python = {
+            -- Ignore all files for analysis to exclusively use Ruff for linting
+            ignore = { "*" },
+          },
+        },
+      })
+
+      lspconfig.solargraph.setup({
+        capabilities = capabilities,
+        init_options = {
+          formatting = false,
+        },
+        settings = {
+          solargraph = { diagnostics = false },
+        },
+      })
+
+      lspconfig.ruby_ls.setup({})
     end,
   },
 }
